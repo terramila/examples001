078589f7f838b6d0163bef1f0a14418d9e2df691
tarana <beshirova@hotmail.com>
2022-09-17 21:41:33 +0300

    Перевод на С#

---
+++ examples006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 2;
+int b = 3;
+int c = 5;
+int d = 8;
+int e = 6;
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
